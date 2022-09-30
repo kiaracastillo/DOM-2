a4f43f7e3156f98b4d13d007a1e2448b2504876f
Kiara Castillo <kiaracastillom@gmail.com>
2022-09-29 20:06:46 -0400

    final

---
+++ js/app.js
@@ -0,0 +1,59 @@
+//Kiara Castillo Magallanes
+//09-29-2022
+//Over and out
+
+//Escriba el marcado y JavaScript para colocar un div 
+//cuadrado en la página (100 px x 100 px), con un fondo azul.
+//Usando onmouseover y onmouseout (en lugar de "onclick"), 
+//cambie el color del div a negro cuando el mouse esté sobre el div, 
+//y vuelva a azul cuando el mouse se vaya.
+
+
+
+
+let colores = document.getElementById("colores");
+colores.style.width = "100px";
+colores.style.height ="100px";
+colores.style.backgroundColor = "#4271bd";
+
+
+function negro (){
+    colores.style.backgroundColor = "#000000";
+}
+function azul (){
+colores.style.backgroundColor = "#4271bd";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
